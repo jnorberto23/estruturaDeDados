4e812b2c50ef8de7e190230a1034f5f16d2840fb
jnorberto23 <joaovictor.norberto@outlook.com>
2023-05-25 12:04:36 -0300

    ✨add remove and search function on static list

--- ed/02.listaEstatica/Lista.c
+++ ed/02.listaEstatica/Lista.c
@@ -103,3 +103,97 @@ int insere_lista_ordenada(Lista *li, struct aluno al)
     li->qtde++;
     return 1;
 }
+
+int remove_lista_final(Lista *li)
+{
+    if (li == NULL)
+    {
+        return 0;
+    }
+    if (li->qtde == 0)
+    {
+        return 0;
+    }
+    li->qtde--;
+    return 1;
+}
+
+int remove_lista_inicio(Lista *li)
+{
+    if (li == NULL)
+    {
+        return 0;
+    }
+    if (li->qtde == 0)
+    {
+        return 0;
+    }
+    int k = 0;
+    for (size_t k = 0; k < li->qtde - 1; k++)
+    {
+        li->dados[k] = li->dados[k + 1];
+    }
+
+    li->qtde--;
+    return 1;
+}
+
+int remove_lista(Lista *li, int mat)
+{
+    if (li == NULL)
+    {
+        return 0;
+    }
+    if (li->qtde == 0)
+    {
+        return 0;
+    }
+    int k, i = 0;
+
+    while (i < li->qtde && li->dados[i].matricula != mat)
+    {
+        i++;
+    }
+    if (i == li->qtde)
+    {
+        return 0;
+    }
+
+    for (size_t k = i; k < li->qtde - 1; k++)
+    {
+        li->dados[k] = li->dados[k + 1];
+    }
+    li->qtde--;
+    return 1;
+}
+
+int consulta_lista_pos(Lista *li, int pos, struct aluno *al)
+{
+    if (li == NULL || pos <= 0 || pos > li->qtde)
+    {
+        return 0;
+    }
+    *al = li->dados[pos - 1];
+    return 1;
+}
+
+int consulta_lista_mat(Lista *li, int mat, struct aluno *al)
+{
+
+    if (li == NULL)
+    {
+        return 0;
+    }
+    int i = 0;
+    while (i < li->qtde && li->dados[i].matricula != mat)
+    {
+        i++;
+    }
+    if (i == li->qtde)
+    {
+        return 0;
+    }
+
+    *al = li->dados[i];
+    return 1;
+}
