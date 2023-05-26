cf33a8d5b3157ee78c6e516d3e86329eb0c5e527
jnorberto23 <joaovictor.norberto@outlook.com>
2023-05-26 11:55:11 -0300

    ✨add dynamic list

--- ed/03.listaDinamica/ListaDinEncad.c
+++ ed/03.listaDinamica/ListaDinEncad.c
@@ -38,25 +38,186 @@ void tamanho_lista(Lista *li)
         return 0;
     }
     int cont = 0;
-    Elem* no = *li;
+    Elem *no = *li;
     while (no != NULL)
     {
         cont++;
-        no = no ->prox;
+        no = no->prox;
     }
 
     return cont;
 }
 
-void lista_vazia(Lista *li){
+void lista_vazia(Lista *li)
+{
     if (li == NULL)
     {
         return 1;
     }
-     if (*li == NULL)
+    if (*li == NULL)
     {
         return 1;
     }
 
     return 0;
+}
+
+int insere_lista_inicio(Lista *li, struct aluno al)
+{
+    if (li == NULL)
+        return 0;
+    Elem *no;
+    no = (Elem *)malloc(sizeof(Elem));
+    if (no == NULL)
+        return 0;
+    no->dados = al;
+    no->prox = (*li);
+    *li = no;
+    return 1;
+}
+
+int insere_lista_final(Lista *li, struct aluno al)
+{
+    if (li == NULL)
+        return 0;
+    Elem *no;
+    no = (Elem *)malloc(sizeof(Elem));
+    if (no == NULL)
+        return 0;
+    no->dados = al;
+    no->prox = NULL;
+    if ((*li) == NULL)
+    {
+        *li = no;
+    }
+    else
+    {
+        Elem *aux;
+        aux = *li;
+        while (aux->prox != NULL)
+        {
+            aux = aux->prox;
+        }
+        aux->prox = no;
+    }
+    return 1;
+}
+
+int insere_lista_ordenada(Lista *li, struct aluno al)
+{
+    if (li == NULL)
+        return 0;
+    Elem *no;
+    no = (Elem *)malloc(sizeof(Elem));
+    if (no == NULL)
+        return 0;
+    no->dados = al;
+    if ((*li) == NULL)
+    {
+        no->prox = NULL;
+        *li = no;
+        return 1;
+    }
+    else
+    {
+        Elem *ant, *atual = *li;
+        while (atual != NULL && atual->dados.matricula < al.matricula)
+        {
+            ant = atual;
+            atual = atual->prox;
+        }
+        if (atual == *li)
+        {
+            no->prox = (*li);
+            *li = no;
+        }
+        else
+        {
+            no->prox = atual;
+            ant->prox = no;
+        }
+        return 1;
+    }
+}
+
+int remove_lista_inicio(Lista *li){
+  if(li == NULL)
+    return 0;
+  if((*li) == NULL) 
+    return 0;
+  Elem *no = *li;
+  *li = no->prox;
+  free(no);
+  return 1;
+}
+
+int remove_lista_final(Lista *li){
+  if(li == NULL)
+    return 0;
+  if((*li) == NULL) 
+    return 0;
+  
+  Elem *ant, *no = *li;
+  while(no->prox != NULL){
+    ant = no;
+    no = no->prox;
+  }
+
+  if(no == (*li)) 
+    *li = no->prox;
+  else
+    ant->prox = no->prox;
+  free(no);
+  return 1;
+}
+
+int remove_lista(Lista *li, int mat){
+  if(li == NULL)
+    return 0;
+  if((*li) == NULL) 
+    return 0;
+  Elem *ant, *no = *li;
+  while(no != NULL && no->dados.matricula != mat){
+    ant = no;
+    no = no->prox;
+  }
+  if(no == NULL) 
+    return 0;
+  if(no == *li) 
+    *li = no->prox;
+  else
+    ant->prox = no->prox;
+  free(no);
+  return 1;
+}
+
+int busca_lista_pos(Lista *li, int pos, struct aluno *al){
+  if(li == NULL || pos <= 0)
+    return 0;
+  Elem *no = *li;
+  int i = 1;
+  while(no != NULL && i < pos){
+    no = no->prox;
+    i++;
+  }
+  if(no == NULL)
+    return 0;
+  else{
+    *al = no->dados;
+    return 1;
+  }
+}
+
+int consulta_lista_mat(Lista *li, int mat, struct aluno *al) {
+  if(li == NULL) return 0;
+  Elem *no = *li;
+  while(no != NULL && no->dados.matricula != mat) {
+    no = no->prox;
+  }
+  if(no == NULL)
+    return 0;
+  else{
+    *al = no->dados;
+    return 1;
+  }
 }
--- ed/03.listaDinamica/ListaDinEncad.h
+++ ed/03.listaDinamica/ListaDinEncad.h
@@ -5,9 +5,17 @@ struct aluno
   float n1, n2, n3;
 };
 
-typedef struct elemento* Lista;
+typedef struct elemento *Lista;
 
-Lista* cria_lista();
-void libera_lista(Lista* li);
+Lista *cria_lista();
+void libera_lista(Lista *li);
 void tamanho_lista(Lista *li);
 void lista_vazia(Lista *li);
+int insere_lista_final(Lista *li, struct aluno al);
+int insere_lista_inicio(Lista *li, struct aluno al);
+int insere_lista_ordenada(Lista *li, struct aluno al);
+int remove_lista(Lista *li, int mat);
+int remove_lista_inicio(Lista *li);
+int remove_lista_final(Lista *li);
+int busca_lista_pos(Lista *li, int pos, struct aluno *al);
+int consulta_lista_mat(Lista *li, int mat, struct aluno *al);
